80d4bf3d76ff04e2a5981aba0603ecd997992119
Yuliia Krasnokutska <yuliia.krasnokutska.cr@gmail.com>
2024-10-06 17:31:04 +0300

    add synopsys

---
+++ lection4.py
@@ -0,0 +1,42 @@
+#-----------------------------Регулярні вирази та розширена робота з рядками-----------------------------------
+# Створення рядків
+this_is_string = "Hi there!"
+
+the_same_string = 'Hi there!'
+
+this_is_string == the_same_string# True
+
+text = """This is first line
+And second line
+Last third line"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
